b9f9b2c291a896cdd57461d85e2fb0574306b27b
cleveraziko <azamat770797@gmail.com>
2022-02-17 00:23:28 +0500

    adding print

--- venv/homework.8.6.py
+++ venv/homework.8.6.py
@@ -19,3 +19,7 @@ elif m <= duration and h > duration:
     days = seconds // 86400
     seconds = seconds % 86400
     print(f"{days} дн, {hours} час, {minutes} мин, {seconds} сек")
+
+
+
+    print("fjdfjds;jfd")
